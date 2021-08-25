90f3320a73a2ef896f01ea01b1518588388c2b9d
Srihari <srih4ri@gmail.com>
2021-08-25 17:22:28 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,29 +1,17 @@
 #EDITOR
 set EDITOR emacsclient
 #PATH
-set -x PATH $PATH $HOME/.rbenv/bin $HOME/bin $HOME/code/tiny_scripts/scripts $GOPATH/bin $HOME/.composer/vendor/bin
+set PATH /home/srihari/.rbenv/bin $PATH
+set PATH /home/srihari/.local/bin $PATH
 
-#GO
-set -x GOPATH $HOME/go
-
-#Node
-set NPM_PACKAGES "$HOME/.npm-packages"
-set -x PATH  $PATH $NPM_PACKAGES/bin
-set NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-
-#Ruby
-set -gx RBENV_ROOT $HOME/.rbenv
-. (rbenv init -|psub)
-
+#ENV
+set -x WKHTMLTOPDF (which wkhtmltopdf)
 #emacs
-alias e='emacsclient -n'
+alias e='emacsclient -n '
 alias j='emacsclient ~/.j/journal.log'
 
-#pacman
-alias pacin='sudo pacman -S'
-
 #rails
-alias dl='tailf log/development.log'
+alias dl='tailf log/dev*.log'
 alias tl='tailf log/test.log'
 
 # git aliases
@@ -32,7 +20,7 @@ alias gco='git checkout'
 alias gd='git diff'
 alias push='git push'
 alias pull='git pull'
-alias gcm='git checkout master'
+alias gcm='git checkout master; or git checkout main'
 alias ggpur='git pull --rebase'
 alias clean_merged='git branch --merged | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d'
 alias grc='git rebase --continue'
@@ -42,16 +30,21 @@ alias gpf='git push --force-with-lease'
 alias create_test_db='env RAILS_ENV=test rake db:create'
 alias whats_wrong='bundle exec rspec spec --fail-fast'
 alias run_all_specs='bundle exec rspec spec'
-function run_spec
-  bundle exec rspec $argv
+function s
+    if test -e bin/spring
+        bin/spring rspec $argv
+    else
+        bundle exec rspec $argv
+    end
 end
+
 #Git helpers
 function current_branch
-  git rev-parse --abbrev-ref HEAD
+    git rev-parse --abbrev-ref HEAD
 end
 
 function set_upstream
-  git branch --set-upstream-to=origin/(current_branch) (current_branch)
+    git branch --set-upstream-to=origin/(current_branch) (current_branch)
 end
 
 #Github helpers
@@ -60,105 +53,191 @@ end
 # gho pulls opens pull requests page
 # gho issues to open issues page
 function gho
-  open https://(git config --get remote.origin.url|sed -e s/.git//g|sed s,:,/,g)/$argv
+    xdg-open https://(git config --get remote.origin.url|sed -e s/.git//g|sed s,:,/,g)/$argv
 end
 
-function pr
-  git push
-  hub pull-request -i $argv[1]
-  gho
+function jira_ticket
+    set jira_base "https://clarkteam.atlassian.net"
+    xdg-open "$jira_base/browse/"(current_branch)
 end
 
-# Fish git prompt
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
-
-function fish_prompt
-  set last_status $status
-
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
-
-  set_color normal
+function first_push
+    git push -u origin (current_branch)
 end
+
+function tailf
+    tail -f $argv
+end
+
+function run_changed_specs
+    for spec in (git diff master --name-only spec)
+        s $spec
+    end
+end
+
 
 function start_agent
-  echo "Initializing new SSH agent ..."
-  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-  echo "succeeded"
-  chmod 600 $SSH_ENV
-  . $SSH_ENV > /dev/null
-  ssh-add
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV
+    . $SSH_ENV >/dev/null
+    ssh-add
 end
 
 function test_identities
-  ssh-add -l | grep "The agent has no identities" > /dev/null
-  if [ $status -eq 0 ]
-    ssh-add
-    if [ $status -eq 2 ]
-      start_agent
+    ssh-add -l | grep "The agent has no identities" >/dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
     end
-  end
 end
 
 #SSH
 setenv SSH_ENV $HOME/.ssh/environment
 
 if [ -n "$SSH_AGENT_PID" ]
-  ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-  if [ $status -eq 0 ]
-    test_identities
-  end
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
 else
-  if [ -f $SSH_ENV ]
-    . $SSH_ENV > /dev/null
-  end
-  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
-  if [ $status -eq 0 ]
-    test_identities
-  else
-    start_agent
-  end
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV >/dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
+    end
 end
-
-
-# Load fishmarks (http://github.com/techwizrd/fishmarks)
-. $HOME/.fishmarks/marks.fish
 
 
 #Sudo helper
 function .runsudo --description 'Run current command line as root'
-  set cursor_pos (echo (commandline -C) + 5 | bc)
-  commandline -C 0
-  commandline -i 'sudo '
-  commandline -C "$cursor_pos"
+    set cursor_pos (echo (commandline -C) + 5 | bc)
+    commandline -C 0
+    commandline -i 'sudo '
+    commandline -C "$cursor_pos"
 end
 
 function fish_user_key_bindings
-  bind \es '.runsudo'
+    bind \es '.runsudo'
 end
 
-funcsave fish_user_key_bindings
+function thin_start
+    bundle exec thin start
+end
+
+function sidekiq_start
+    bundle exec sidekiq
+end
+
+function restart_unicorn
+    kill -HUP (cat /tmp/unicorn.milky_way_development.pid)
+end
+
+function zr
+    zeus rake $argv
+end
+
+function zc
+    zeus c
+end
+
+function mg
+    mix deps.get
+end
+
+function tbp
+    tmux break-pane
+end
+
+function this_branch_param
+    git branch ^/dev/null | grep \* | sed 's/* //' | sed 's/\//-/' | sed 's/_/-/g'
+end
+
+function psclark
+    psql -A postgresql://clark:clark@localhost/optisure_development $argv
+end
+
+function update_app
+    git checkout master; and \
+        git pull --rebase; and \
+        bundle; and \
+        bundle exec rake db:migrate; and \
+        yarn
+end
+
+status --is-interactive; and rbenv init - | source
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+export NODE_OPTIONS=--max-old-space-size=4096
+
+
+## Clark K8s helpers
+##
+
+function get_pod_with_name --argument pod_name --description "Get pod with a particular name"
+    kubectl get pods -o json -l app.kubernetes.io/name=application-backend | jq -r '.items[0].metadata.name'
+end
+
+function execute_command_on_pod --argument pod_name cmd --description "Execute a command on given pod name"
+    echo "Executing $cmd on $pod_name"
+    kubectl exec -it $pod_name -- $cmd
+end
+
+function k8s_rails_c
+    echo "Getting pod for application backend"
+    set pod (get_pod_with_name 'application_backend')
+    echo "Executing /bin/sh on $pod"
+    execute_command_on_pod $pod '/bin/sh'
+end
+
+function k8_login_to_cluster --argument cluster
+    aws --profile saml eks --region eu-central-1 update-kubeconfig --name $cluster
+end
+
+function k8_login_staging
+    k8_login_to_cluster staging
+end
+
+function k8_login
+    k8_login_to_cluster development
+end
+
+function lsj
+    for file in (git diff master --name-only '*.ts' '*.js')
+        eslint $file
+    end
+end
+
+function lsjf
+    for file in (git diff master --name-only '*.ts' '*.js')
+        eslint --fix $file
+    end
+end
+
+function lsr
+    for file in (git diff master --name-only '*.rb')
+        echo "Checking $file"
+        rubocop $file
+    end
+end
+
+function lsrf
+    for file in (git diff master --name-only '*.rb')
+        echo "Checking $file"
+        rubocop -A $file
+    end
+end
+
+function a
+    ag --ignore='dist' $argv
+end
+
+set PATH ./node_modules/.bin $PATH
