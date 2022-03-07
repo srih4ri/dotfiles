4f986235b8e4adbaaa2069c892acc3bea88da56d
Srihari K <srihari.kookal@clark.de>
2022-03-07 20:44:28 +0100

    Rearrange files

--- setup.rb
+++ setup.rb
@@ -2,27 +2,25 @@
 require 'fileutils'
 
 dot_files_map = {
-  'config.fish' => '~/.config/fish/config.fish',
-  '.spacemacs' => '~/.spacemacs',
-  'scripts/deploy.sh' => '~/scripts/deploy.sh',
-  '.irbrc' => '~/.irbrc',
-  'rc.xml' => '~/.config/openbox/rc.xml',
-  '.gitconfig' => '~/.gitconfig'
-}
+  "config/fish/config.fish" => "~/.config/fish/config.fish",
+  "config/ruby/irbrc" => "~/.irbrc",
+  "config/git/gitconfig" => "~/.gitconfig",
+  "config/emacs/doom/" => "~/.doom.d/"
+  }
 
 def create_folder_and_copy(source,dest)
   FileUtils.mkdir_p(File.expand_path(File.dirname(dest)))
-  puts `cp -rv #{source} #{dest}`
+  puts "cp -rv #{source} #{dest}"
 end
 
 case ARGV[0]
-when 'install'
-  puts 'Installing files from this folder'
+when "install"
+  puts "Installing files from this folder"
   dot_files_map.each do |source, dest|
     create_folder_and_copy(source,dest)
   end
-when 'update'
-  puts 'Updating this folder'
+when "update"
+  puts "Updating this folder"
   dot_files_map.each do |dest, source|
     create_folder_and_copy(source,dest)
   end
