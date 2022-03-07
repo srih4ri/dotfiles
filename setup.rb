c1ef20b8334dd13205173fb17b4326eee2200e0e
Srihari Kookal <srih4ri@gmail.com>
2022-03-07 20:50:42 +0100

    Update install script

--- setup.rb
+++ setup.rb
@@ -5,12 +5,15 @@ dot_files_map = {
   "config/fish/config.fish" => "~/.config/fish/config.fish",
   "config/ruby/irbrc" => "~/.irbrc",
   "config/git/gitconfig" => "~/.gitconfig",
-  "config/emacs/doom/" => "~/.doom.d/"
-  }
+  "config/emacs/doom/custom.el" => "~/.doom.d/custom.el",
+  "config/emacs/doom/config.el" => "~/.doom.d/config.el",
+  "config/emacs/doom/init.el" => "~/.doom.d/init.el",
+  "config/emacs/doom/packages.el" => "~/.doom.d/packages.el"
+}
 
 def create_folder_and_copy(source,dest)
   FileUtils.mkdir_p(File.expand_path(File.dirname(dest)))
-  puts "cp -rv #{source} #{dest}"
+  puts `cp -v #{source} #{dest}`
 end
 
 case ARGV[0]
