2c8d538252adfc35f25abb686b7ffae080d6eb84
Srihari Kookal <srih4ri@gmail.com>
2022-03-22 08:24:28 +0100

    Add diff command

--- setup.rb
+++ setup.rb
@@ -1,35 +1,41 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'fileutils'
 
 dot_files_map = {
-  "config/fish/config.fish" => "~/.config/fish/config.fish",
+  'config/fish/config.fish' => '~/.config/fish/config.fish',
 
-  "config/ruby/irbrc" => "~/.irbrc",
+  'config/ruby/irbrc' => '~/.irbrc',
 
-  "config/git/gitconfig" => "~/.gitconfig",
+  'config/git/gitconfig' => '~/.gitconfig',
 
-  "config/emacs/doom/custom.el" => "~/.doom.d/custom.el",
-  "config/emacs/doom/config.el" => "~/.doom.d/config.el",
-  "config/emacs/doom/init.el" => "~/.doom.d/init.el",
-  "config/emacs/doom/packages.el" => "~/.doom.d/packages.el",
+  'config/emacs/doom/custom.el' => '~/.doom.d/custom.el',
+  'config/emacs/doom/config.el' => '~/.doom.d/config.el',
+  'config/emacs/doom/init.el' => '~/.doom.d/init.el',
+  'config/emacs/doom/packages.el' => '~/.doom.d/packages.el',
 
-  "config/tmux/tmux.conf" => "~/.tmux.conf"
+  'config/tmux/tmux.conf' => '~/.tmux.conf'
 }
 
-def create_folder_and_copy(source,dest)
+def create_folder_and_copy(source, dest)
   FileUtils.mkdir_p(File.expand_path(File.dirname(dest)))
   puts `cp -v #{source} #{dest}`
 end
 
 case ARGV[0]
-when "install"
-  puts "Installing files from this folder"
+when 'install'
+  puts 'Installing files from this folder'
   dot_files_map.each do |source, dest|
-    create_folder_and_copy(source,dest)
+    create_folder_and_copy(source, dest)
   end
-when "update"
-  puts "Updating this folder"
+when 'update'
+  puts 'Updating this folder'
   dot_files_map.each do |dest, source|
-    create_folder_and_copy(source,dest)
+    create_folder_and_copy(source, dest)
+  end
+when 'diff'
+  dot_files_map.each do |source, dest|
+    `diff #{source} #{dest}`
   end
 end
